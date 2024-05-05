b194817e57c47bd8a8d34289180284901ad856d5
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-05-05 18:33:18 +0300

    Establish basic gateway setup

--- api-gateway/src/main/java/org/chainoptim/apigateway/GatewayConfig.java
+++ api-gateway/src/main/java/org/chainoptim/apigateway/GatewayConfig.java
@@ -11,10 +11,12 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("organizations-route", r -> r.path("/api/v1/organizations/**")
-                        .uri("lb://chainoptim-core"))
-                .route("notifications-route", r -> r.path("/api/v1/notification/**")
-                        .uri("lb://chainoptim-notifications"))
-                .build();
+            .route("organizations-route", r -> r.path("/api/v1/organizations/**")
+                    .uri("http://chainoptim-core:8080"))
+            .route("products-route", r -> r.path("/api/v1/products/**")
+                .uri("http://chainoptim-core:8080"))
+            .route("notifications-route", r -> r.path("/api/v1/notifications/**")
+                    .uri("http://chainoptim-notifications:8081"))
+            .build();
     }
 }
